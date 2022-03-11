f1910f5b2ed431a4d64dba2fee3456ec65903017
1342470 <kylewoollam@gmail.com>
2022-03-11 11:02:09 +0000

    added post method

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+/* GET home page. */
+// router.post('/test', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+router.post('/test', function (req, res, next) {
+  fs.writeFile('writefile.txt', 'file contents', 'utf8', (err) => {
+    console.log("Wrote to file writefile.txt");
+  });
   res.render('index', { title: 'Express' });
 });
 
